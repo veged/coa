117560fd38060707b7bd3d3b9c6b56c2e35477a0
Jordan Yaker <jordan.e.yaker@gmail.com>
2016-12-14 21:41:27 -0500

    Corrected the loader to not be inline.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require(process.env.COVER? './lib-cov' : './lib');
+var lib;
+
+if (process.env.COVER) {
+  lib = require('./lib-cov');
+} else {
+  lib = require('./lib');
+}
+
+module.exports = lib;
